473a4e97e7bc3b8b744ab87599ca39ad14b6f6ef
MUHAMMEDSAHALCP <muhammedsahalsh>
2022-06-28 00:59:12 +0530

    home page modified

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: (Colors.white),
-      child: const Text("Home page screen"),
+      child: const Text("main page"),
     );
   }
 }
