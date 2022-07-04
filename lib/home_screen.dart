da81dea2832fb91930133220e045751e755bb257
MUHAMMED SAHAL CP <86265671+MUHAMMEDSAHALCP@users.noreply.github.com>
2022-07-04 17:02:31 +0530

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: (Colors.white),
-      child: const Text("second say kajwnfdoasjfnowanfeowjenfeowjfn"),
+      child: const Text("sahal"),
     );
   }
 }
